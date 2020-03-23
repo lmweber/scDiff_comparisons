0078baeaf3664d1eba1d52d577aa2f40062fa2f0
Lukas Weber <lukas.weber.edu@gmail.com>
2020-03-22 21:10:02 -0400

    update example script

--- scDiff_example.R
+++ scDiff_example.R
@@ -1,23 +1,23 @@
-#######################################
-# Example script demonstrating 'scDiff'
-# Lukas Weber, Feb 2020
-#######################################
+######################################
+# Example script for 'scDiff' analyses
+# Lukas Weber, Mar 2020
+######################################
 
 
 # ------------
-# installation
+# Installation
 # ------------
 
 # install from source (note: requires compiler)
 #install.packages("scDiff_0.99.tar.gz", repos = NULL, type = "source")
 
 
-# -----------
-# run example
-# -----------
+# --------------
+# Simple example
+# --------------
 
-# example demonstrating differential testing using scDiff with dataset from
-# HDCytoData package
+# simple example showing how to perform differential testing using scDiff, with
+# dataset from HDCytoData package
 
 library(scDiff)
 library(HDCytoData)
@@ -37,7 +37,7 @@ sample_names <- levels(rowData(d_SE)$sample_id)
 sample_names
 
 # subset and put back together into original object format
-rd <- data.frame()
+rd <- DataFrame()
 cd <- colData(d_SE)
 md <- metadata(d_SE)
 ad <- c()
@@ -65,7 +65,10 @@ dim(d_SE_sub)
 # transform data: using standard transform asinh(x/5) for CyTOF data (similar to
 # log, but linear near zero and allows negative values)
 
-ad_transf <- asinh(assay(d_SE_sub) / 5)
+cols_transf <- colData(d_SE_sub)$marker_class != "none"
+cofactor <- 5
+ad_transf <- assay(d_SE_sub)
+ad_transf[, cols_transf] <- asinh(ad_transf[, cols_transf, drop = FALSE] / cofactor)
 assay(d_SE_sub) <- ad_transf
 
 summary(assay(d_SE_sub))
@@ -73,7 +76,7 @@ summary(assay(d_SE_sub))
 
 # run scDiff
 
-permute_samples <- test_CYTOF(
+res_scDiff <- test_CYTOF(
   d_SE_sub, 
   perm_cells = FALSE, 
   logarithm = FALSE, 
@@ -84,7 +87,32 @@ permute_samples <- test_CYTOF(
 
 
 # output
-dim(permute_samples)
-head(permute_samples)
+dim(res_scDiff)
+head(res_scDiff)
+#View(res_scDiff)
+
+
+# --------------
+# Interpretation
+# --------------
+
+# Since we are testing for differential states (DS) within cell populations, for
+# CyTOF data, it only makes sense to test 'state' markers. Therefore, we subset
+# the final results to show 'state' markers only.
+
+# However, this may break the p-value adjustments for multiple testing.
+
+markers_type <- colData(d_SE)$marker_name[colData(d_SE)$marker_class == "type"]
+markers_state <- colData(d_SE)$marker_name[colData(d_SE)$marker_class == "state"]
+
+markers_type
+markers_state
+
+res_scDiff <- res_scDiff[res_scDiff$gene %in% markers_state, , drop = FALSE]
+rownames(res_scDiff) <- NULL
+
+dim(res_scDiff)
+head(res_scDiff)
+#View(res_scDiff)
 
 
